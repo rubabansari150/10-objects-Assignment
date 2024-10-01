4d8c391628b972fb0acd3ffe2a4e2ef2426f5413
rubabansari150 <rubabansari21992@gmail.com>
2024-10-01 17:12:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+// Card 1
+var namaz = {
+    fajar: "Early Morning",
+    zhohar: "Afternoon",
+    asar: "Evening",
+    mgrib: "Starting Night",
+    isha: "Full Night"
+};
+
+document.getElementById("card_1").innerHTML = "Fajar: " + namaz.fajar + "<br>" + "Zohar: " + namaz.zhohar+ "<br>" + "Asar: " + namaz.asar + "<br>" + "migrib: " + namaz.mgrib + "<br>" + "Isha: " + namaz.isha;
+
+// Card 2
+var languagesOfPakistan = {
+    sindh: "Sindhi",
+    punjab: "Punjabi",
+    baluchistan: "Blochi",
+    sarhad: "pashto"
+    };
+
+document.getElementById("card_2").innerHTML = "Sindh: " + languagesOfPakistan.sindh  + "<br>" + "Punjab " + languagesOfPakistan.punjab + "<br>" + "Baluchistan: " + languagesOfPakistan.baluchistan+ "<br>" + "Sarhad " + languagesOfPakistan.sarhad;
+
+// Card 3
+var fruitsColors = {
+    mango: "Yellow",
+    orange: "Orange",
+    stawberry: "Red",
+    grappes: "Green",
+    apple: "Red"
+   
+};
+
+document.getElementById("card_3").innerHTML = "Mango: " + fruitsColors.mango + "<br>" + "Orange: " + fruitsColors.orange+ "<br>" + "Stawberry: " + fruitsColors.stawberry + "<br>" + "Grappes: " + fruitsColors.grappes + "<br>" + "Apple: " + fruitsColors.apple;
+
+// Card 4
+var vagetableColors = {
+    potato: "Yellow brown",
+    carrot: "Dark green",
+    radish: "Red",
+    onion: "Dark pink",
+    lemon: "Yellow"
+};
+
+document.getElementById("card_4").innerHTML = "Potato: " + vagetableColors.potato + "<br>" + "Carrot: " + vagetableColors.carrot+ "<br>" + "Radish: " +vagetableColors.radish + "<br>" + "Onion: " + vagetableColors.onion + "<br>" + "Lemon: " + vagetableColors.lemon;
+
+// Card 5
+var fruiteJuciesColors = {
+    red: "Carrot jucies",
+    yellow: "Mango juices",
+    purple: "Red grappes juices",
+    orange: "Orange juices",
+    pink: "Stawberry juices"
+};
+
+document.getElementById("card_5").innerHTML = "Red: " + fruiteJuciesColors.red + "<br>" + "Yellow: " + fruiteJuciesColors.yellow+ "<br>" + "Purple: " + fruiteJuciesColors.purple+ "<br>" + "Orange: " + fruiteJuciesColors.orange + "<br>" + "Pink: " + fruiteJuciesColors.pink;
+
+// Card 6
+var laptop = {
+    brand: "Apple",
+    model: "MacBook Pro",
+    screenSize: "14 inch",
+    processor: "M1 Pro",
+    ram: "16GB",
+    weight: "3.5 lbs"
+};
+
+document.getElementById("card_6").innerHTML = "brand: " + laptop.brand + "<br>" + "Modle: " + laptop.model+ "<br>" + "ScreenSize: " + laptop.screenSize + "<br>" + "Processor: " +laptop.processor + "<br>" + "Ram: " + laptop.ram + "<br>" + "Weight: " + laptop.weight;
+
+// Card 7
+var Quran = {
+    title: "The Holy Quran",
+    language: "Arabic",
+    binding: "Leather",
+    price: "$15.99",
+    surahs: 114
+};
+
+document.getElementById("card_7").innerHTML = "title: " + Quran.title + "<br>" + "Language: " + Quran.language+ "<br>" + "Binding: " + Quran.binding + "<br>" + "Price: " + Quran.price + "<br>" + "Surahs: " + Quran.surahs;
+
+// Card 8
+var breakFast = {
+    mainDish: "Paratha",
+    drink: "Orange Juice",
+    EggOmlate: [ "Eggs", "Milk", "Sugar", "Butter"],
+};
+
+document.getElementById("card_8").innerHTML = "Main Dish: " + breakFast.mainDish + "<br>" + "drink: " + breakFast.drink+ "<br>" + "Sweet Egg Omlate: " +breakFast.EggOmlate;
+
+// Card 9
+var ourNationFlag = {
+    country: "Pakistan",
+    colors: ["Green", "White"],
+    dateOfInvention: "August 11, 1947",
+    designer: "Syed Amir-uddin Kedwaii"
+};
+
+document.getElementById("card_9").innerHTML = "Country: " + ourNationFlag.country + "<br>" + "Color " + ourNationFlag.colors + "<br>" + "Date of invetion: " + ourNationFlag.dateOfInvention + "<br>" + "Designer: " + ourNationFlag.designer + "<br>" + "Isha: " + namaz.isha;
+
+// Card 10
+var makeUp = {
+    productName: "Foundation",
+    brand: "abc",
+    Use: "Face",
+    expairyDate: "2025-05-15"
+};
+
+document.getElementById("card_10").innerHTML = "Product: " + makeUp.productName + "<br>" + "Brand: " + makeUp.brand+ "<br>" + "Recommended Use: " + makeUp.Use+ "<br>" + "Expairy: " + makeUp.expairyDate;
